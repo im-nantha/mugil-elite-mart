3b8b45728b97ef12ecf3580a579f3ae4d8dedc7d
Nanthakumar R <rnantha2002@gmail.com>
2025-09-01 00:35:09 +0530

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/mugil-elite-mart/",
   plugins: [react()],
+  base: "/mugil-elite-mart/",
 });
