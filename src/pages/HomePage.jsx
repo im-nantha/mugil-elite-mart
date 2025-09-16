3172c10261b294d177c9341aa8751e002cd7b75d
Nanthakumar R <rnantha2002@gmail.com>
2025-09-16 19:18:31 +0530

    latest changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import Features from "../components/Features/Features";
 import IntroBanner from "../components/IntroBanner/IntroBanner";
 import ProductTabs from "../components/ProductTabs/ProductTabs";
 import StaticBanner from "../components/StaticBanner/StaticBanner";
-import ShopByCategories from "../components/ShopByCategories/ShopByCategories";
+import ShopByCategories from "../components/ShopByCategories/MugilGallery";
 import Testimonials from "../components/Testimonials/Testimonials";
 import Gallery from "../components/Gallery/Gallery";
 import Faq from "../components/Faq/Faq";
