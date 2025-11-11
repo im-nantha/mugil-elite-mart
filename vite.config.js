87f15983cbecd0ccb0fba68aa1072170ad155d27
Nanthakumar R <rnantha2002@gmail.com>
2025-11-11 23:28:03 +0530

    latest changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === "serve" ? "/mugil-elite-mart/" : "/",
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: "/",
 });
