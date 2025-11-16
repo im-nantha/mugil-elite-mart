c9c4dfb0cfbf90656aa911a9775dd2e60d03cadb
Nanthakumar R <rnantha2002@gmail.com>
2025-11-16 19:17:32 +0530

    latest changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/mugil-elite-mart/",
   plugins: [react()],
   build: {
     rollupOptions: {
