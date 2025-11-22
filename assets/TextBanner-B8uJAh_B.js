cb1600bcad6ae4cb7d9c04755177f24c52d9058e
Nanthakumar R <rnantha2002@gmail.com>
2025-11-22 20:18:02 +0530

    Updates

---
+++ assets/TextBanner-B8uJAh_B.js
@@ -0,0 +1,1 @@
+import{j as e}from"./Header-34Ul-4_I.js";function s({bannerText:n}){return e.jsx("section",{className:"text-banner-wrapper",children:e.jsx("div",{className:"text-banner",children:e.jsx("div",{className:"container",children:e.jsx("div",{className:"text-center text-banner-width",children:e.jsx("h1",{className:"title",children:n})})})})})}export{s as T};
